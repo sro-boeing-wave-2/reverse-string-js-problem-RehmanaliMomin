956498ebb9c86985b9ec1b1d5c56820a5c2b6eb0
rehman <rehmanmomin@gmail.com>
2018-07-10 10:32:37 +0530

    Assignment done, with one line-break linting issue

--- reverse-using-built-in-function.js
+++ reverse-using-built-in-function.js
@@ -1,5 +1,11 @@
 // The intent of this file is to reverse
 // the string using built in functions.
-const reverse = () => {};
+
+function reverse(str) {
+  const array = str.split('');
+  const reverseArray = array.reverse();
+  const joinArray = reverseArray.join('');
+  return joinArray;
+}
 
 module.exports = reverse;
--- reverse-using-iterators.js
+++ reverse-using-iterators.js
@@ -1,4 +1,13 @@
 // The intent of this file is to reverse the string using iterators (for, foreach)
-const reverse = () => {};
+
+
+const reverse = (str) => {
+  let j = str.length - 1;
+  let ans = '';
+  for (j = str.length - 1; j >= 0; j -= 1) {
+    ans += str[j];
+  }
+  return ans;
+};
 
 module.exports = reverse;
